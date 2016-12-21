4025a867c9d4366df33d21f9d59cbdd5dc60920f
mesour <matous.nemec@gmail.com>
2016-12-21 21:26:52 +0100

    minor change

--- example/index.php
+++ example/index.php
@@ -21,15 +21,12 @@ if (isset($_GET['m_do'])) {
 		<br><br><br>
 
 		<div class="modal fade" tabindex="-1" role="dialog" data-mesour-modal="mesourApp-testModal" data-show="false" data-mesour-show="true" data-keyboard="true" data-backdrop="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Test modal</h4></div><div class="modal-body" mesour-modal-body="true" data-ajax-loading="mesourApp-testModal-modalBody" data-is-cached="true"></div><div class="modal-footer"><button class="btn btn-default" role="button" data-dismiss="modal">Close</button><a class="btn btn-primary" role="button">Save</a></div></div></div></div>
-		<div class="container">
 
-			<hr>
+		<hr>
 
-			<a href="#" class="btn btn-primary btn-lg" data-modal-show="mesourApp-testModal">
-				Launch modal
-			</a>
-
-		<br><br><br><br><br><br>
+		<a href="#" class="btn btn-primary btn-lg" data-modal-show="mesourApp-testModal">
+			Launch modal
+		</a>
 	</div>
 
 	<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
